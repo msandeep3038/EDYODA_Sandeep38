7c3db48738b11531b52064a8d50b4d4daf288508
msandeep3038 <135109934+msandeep3038@users.noreply.github.com>
2023-06-15 16:00:27 +0530

    Add files via upload
    How can we access private data from one class to other class? Explain and write down the code for it?

---
+++ Student.java
@@ -0,0 +1,15 @@
+package javaAssignment;
+
+public class Student {
+    private String name;
+
+    public String getName()
+    {
+        return getName();
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+package javaAssignment;
+
+public class Test {
+    public void main(String[] args)
+    {
+        Student s=new Student();
+        s.setName("John");
+        System.out.printf(s.getName());
+
+    }
+}
